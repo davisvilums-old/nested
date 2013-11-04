3c1a1b464d99ce3dc0568b900ad1f67d3d6c44c8
Davis Vilums <davisvilums@gmail.com>
2013-11-04 15:18:04 +0000

    Revert "Revert "Revert "Replacing smart resize to jQuery resize"""
    This reverts commit 8e5ffd578675d4c0d636cd6846615cd85e8a8d22.

--- jquery.nested.js
+++ jquery.nested.js
@@ -10,6 +10,31 @@
 
 // Debouncing function from John Hann
 // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+// Copy pasted from http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/
+
+(function ($, sr) {
+    var debounce = function (func, threshold, execAsap) {
+        var timeout;
+        return function debounced() {
+            var obj = this,
+                args = arguments;
+
+            function delayed() {
+                if (!execAsap) func.apply(obj, args);
+                timeout = null;
+            };
+            if (timeout) clearTimeout(timeout);
+            else if (execAsap) func.apply(obj, args);
+
+            timeout = setTimeout(delayed, threshold || 150);
+        };
+    }
+    jQuery.fn[sr] = function (fn) {
+        return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr);
+    };
+
+})(jQuery, 'smartresize');
+
 // Simple count object properties
 
 if (!Object.keys) {
@@ -63,7 +88,8 @@ if (!Object.keys) {
             this._update = true;
             this.maxy = new Array();
 
-            $(window).resize(function(){
+            // add smartresize
+            $(window).smartresize(function () {
                 container.resize();
             });
 
